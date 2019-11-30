28f1a8c8ff593e0b2bf4d781fb37fb4671683124
Sunhwa Cheon <Sunhwa@CheonSunhwaui-MacBook-Air.local>
2019-11-30 13:59:02 -0500

    11-30

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const product0 = {
+/*const product0 = {
     imgsrc :`img/can6.jpg`,
     name: `Simply Nourish® Kitten Chicken Stew`,
     discription: `Simply Nourish® Shreds & Flake Stew Wet Cat Food - Natural, Grain Free`,
@@ -81,7 +81,141 @@ const product0 = {
     size3: `150g`,
     priceSale: `1.99`,
     priceRegular: `2.11`
-  };
+  };*/
+
+  const products = [
+    {
+    imgsrc :`img/can1.jpg`,
+    name: `CRAVE™ Perfect Portions Adult Cat Food`,
+    discription: `Real chicken is the first ingredient, and this protein-rich recipe provides cats with energy and supports lean bodies`,
+    flavor1: `Chicken`,
+    flavor2: `Turkey`,
+    flavor3: `Chicken & Beef`,
+    size1: `32g`,
+    size2: `79g`,
+    size3: `150g`,
+    priceSale: `1.99`,
+    priceRegular: `2.11`
+  },
+  {
+    imgsrc :`img/can2.jpg`,
+    name: `Authority® Advanced Wellness Shredded`,
+    discription: `This delicious wet food features the great taste of chicken in each and every bite`,
+    flavor1: `Mackerel`,
+    flavor2: `Salmon`,
+    flavor3: `Tuna`,
+    size1: `15.5g`,
+    size2: `35g`,
+    size3: `70g`,
+    priceSale: `1.19`,
+    priceRegular: `1.99`
+  },
+  {
+    imgsrc :`img/can3.jpg`,
+    name: `Royal Canin® Feline Health Nutrition`,
+    discription: `A uniquely balanced blend of vitamins and minerals promotes your pet's optimal health and wellness`,
+    flavor1: `Sensitive Digestion`,
+    flavor2: `Beauty Chunk`,
+    flavor3: `Nutrition Chunk`,
+    size1: `85g`,
+    size2: `120g`,
+    size3: `none`,
+    priceSale: `1.89`,
+    priceRegular: `1.99`
+  },
+  {
+    imgsrc :`img/can4.jpg`,
+    name: `Wellness® CORE® Tiny Tasters Wet Cat Food`,
+    discription: `Based on the nutritional philosophy that cats, given their primal ancestry, thrive on a diet mainly comprised of meat`,
+    flavor1: `Beef`,
+    flavor2: `Tuna & Chicken`,
+    flavor3: `Duck`,
+    size1: `60g`,
+    size2: `85g`,
+    size3: `none`,
+    priceSale: `1.99`,
+    priceRegular: `2.19`
+  },
+  {
+    imgsrc :`img/can5.jpg`,
+    name: `Simply Nourish® Pate Wet Adult Cat Food`,
+    discription: `Perfect choice nutritionally for your adult cat`,
+    flavor1: `Salmon`,
+    flavor2: `Shrimp`,
+    flavor3: `Tuna & Shrimp`,
+    size1: `85g`,
+    size2: `155g`,
+    size3: `350g`,
+    priceSale: `2.35`,
+    priceRegular: `2.55`
+  },
+  {
+    imgsrc :`img/can6.jpg`,
+    name: `Simply Nourish® Kitten Chicken Stew`,
+    discription: `Simply Nourish® Shreds & Flake Stew Wet Cat Food - Natural, Grain Free`,
+    flavor1: `Chicken`,
+    flavor2: `Salmon`,
+    flavor3: `Turkey`,
+    size1: `79g`,
+    size2: `129g`,
+    size3: ``,
+    priceSale: `1.99`,
+    priceRegular: `2.05`
+  },
+  {
+    imgsrc :`img/can7.jpg`,
+    name: `Only Natural Pet® 5 Ingredient Soup Cat Food Topper`,
+    discription: `You won't find any grains, thickeners or artificial ingredients in these tasty items! `,
+    flavor1: `Chicken & Whitefish`,
+    flavor2: `Pork`,
+    flavor3: `Turkey & Chicken`,
+    size1: `118g`,
+    size2: `258g`,
+    size3: ``,
+    priceSale: `5.49`,
+    priceRegular: `5.99`
+  },
+  {
+    imgsrc :`img/can8.jpg`,
+    name: `Iams® Perfect Portions`,
+    discription: `Healthy Kitten Food`,
+    flavor1: `Chicken`,
+    flavor2: `Shrimp`,
+    flavor3: `Tuna & Shrimp`,
+    size1: `75g`,
+    size2: `155g`,
+    size3: `350g`,
+    priceSale: `1.04`,
+    priceRegular: `1.24`
+  },
+  {
+    imgsrc :`img/can9.jpg`,
+    name: `Beyond® Grain Free Adult Cat Food`,
+    discription: `Duck is the #1 ingredient in this wet cat food, and it's accented with wholesome vegetables, including sweet potatoes and carrots`,
+    flavor1: `Duck & Sweet`,
+    flavor2: `Shrimp`,
+    flavor3: `Tuna & Shrimp`,
+    size1: `85g`,
+    size2: `155g`,
+    size3: `350g`,
+    priceSale: `1.59`,
+    priceRegular: `2.55`
+  },
+  {
+    imgsrc :`img/can10.jpg`,
+    name: `Merrick® Backcountry ™ Pate Adult`,
+    discription: `This delicious food is made using all-natural ingredients you can feel good about serving your cat,`,
+    flavor1: `Chicken`,
+    flavor2: `Rabbit`,
+    flavor3: `Tuna & Salmon`,
+    size1: `85g`,
+    size2: `155g`,
+    size3: `350g`,
+    priceSale: `3.39`,
+    priceRegular: `3.39`
+  }
+
+  ]
 
 
   function getProductAsHtmlString(product) {
@@ -128,9 +262,36 @@ const product0 = {
   </article>`;
   }
 
-document.getElementById('products').innerHTML = getProductAsHtmlString(product5)
-document.getElementById('products').innerHTML += getProductAsHtmlString(product4)
-document.getElementById('products').innerHTML += getProductAsHtmlString(product3)
-document.getElementById('products').innerHTML += getProductAsHtmlString(product2)
-document.getElementById('products').innerHTML += getProductAsHtmlString(product1)
-document.getElementById('products').innerHTML += getProductAsHtmlString(product0)
+document.getElementById('products').innerHTML = getProductAsHtmlString(products[0])
+document.getElementById('products').innerHTML += getProductAsHtmlString(products[1])
+document.getElementById('products').innerHTML += getProductAsHtmlString(products[2])
+document.getElementById('products').innerHTML += getProductAsHtmlString(products[3])
+document.getElementById('products').innerHTML += getProductAsHtmlString(products[4])
+document.getElementById('products').innerHTML += getProductAsHtmlString(products[5])
+document.getElementById('products').innerHTML += getProductAsHtmlString(products[6])
+document.getElementById('products').innerHTML += getProductAsHtmlString(products[7])
+document.getElementById('products').innerHTML += getProductAsHtmlString(products[8])
+document.getElementById('products').innerHTML += getProductAsHtmlString(products[9])
+
+
+function renderProducts(arrToRender) {
+  const arrOfHtmlproducts = arrToRender.map(getProductAsHtmlString);
+  const strOfHtmlproducts = arrOfHtmlproducts;
+  document.getElementById('products').innerHTML = strOfHtmlproducts;  
+}
+
+/*document.getElementById('products').innerHTML = getProductAsHtmlString(product[0])
+document.getElementById('products').innerHTML += getProductAsHtmlString(product[1])
+
+/*const arrOfHtmlProducts = product.map(getProductAsHtmlString);
+const strOfHtmlProducts = arrOfHtmlProducts.join('\n');
+document.getElementById('products').innerHTML = strOfHtmlProducts;*/
+
+
+function toggleCourseLayout() {
+  document.getElementById('products').classList.toggle('grid-view');
+}
+
+renderProducts(products);
+document.getElementById('toggleView').addEventListener('click', toggleCourseLayout);
+
