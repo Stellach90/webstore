64f33be0d3e42cd267ee4a6e45748674ee970a59
Sunhwa Cheon <Sunhwa@CheonSunhwaui-MacBook-Air.local>
2019-12-06 10:14:26 -0500

    12-06

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 
 
+  
+
+  
   const products = [
     {
     imgsrc :`img/can1.jpg`,
@@ -11,7 +14,7 @@
     size1: `32g`,
     size2: `79g`,
     size3: `150g`,
-    priceSale: `1.99`,
+    priceSale: 1.99,
     priceRegular: `2.11`,
     category: `plastic container`
   },
@@ -25,7 +28,7 @@
     size1: `15.5g`,
     size2: `35g`,
     size3: `70g`,
-    priceSale: `1.19`,
+    priceSale: 1.19,
     priceRegular: `1.99`,
     category: `plastic container`
   },
@@ -39,7 +42,7 @@
     size1: `85g`,
     size2: `120g`,
     size3: `none`,
-    priceSale: `1.89`,
+    priceSale: 1.89,
     priceRegular: `1.99`,
     category: `pouch`
   },
@@ -53,7 +56,7 @@
     size1: `60g`,
     size2: `85g`,
     size3: `none`,
-    priceSale: `1.99`,
+    priceSale: 1.99,
     priceRegular: `2.19`,
     category: `pouch`
   },
@@ -67,7 +70,7 @@
     size1: `85g`,
     size2: `155g`,
     size3: `350g`,
-    priceSale: `2.35`,
+    priceSale: 2.35,
     priceRegular: `2.55`,
     category: `can`
   },
@@ -81,7 +84,7 @@
     size1: `79g`,
     size2: `129g`,
     size3: ``,
-    priceSale: `1.99`,
+    priceSale: 1.99,
     priceRegular: `2.05`,
     category: `plastic container`
   },
@@ -95,7 +98,7 @@
     size1: `118g`,
     size2: `258g`,
     size3: ``,
-    priceSale: `5.49`,
+    priceSale: 5.49,
     priceRegular: `5.99`,
     category: `plastic container`
   },
@@ -109,7 +112,7 @@
     size1: `75g`,
     size2: `155g`,
     size3: `350g`,
-    priceSale: `1.04`,
+    priceSale: 1.04,
     priceRegular: `1.24`,
     category: `plastic container`
   },
@@ -123,7 +126,7 @@
     size1: `85g`,
     size2: `155g`,
     size3: `350g`,
-    priceSale: `1.59`,
+    priceSale: 1.59,
     priceRegular: `2.55`,
     category: `can`
   },
@@ -137,7 +140,7 @@
     size1: `85g`,
     size2: `155g`,
     size3: `350g`,
-    priceSale: `3.39`,
+    priceSale: 3.39,
     priceRegular: `3.39`,
     category: `can`
   },
@@ -151,7 +154,7 @@
     size1: `90g`,
     size2: `110g`,
     size3: `220g`,
-    priceSale: `1.79`,
+    priceSale: 1.79,
     priceRegular: `1.89`,
     category: `pouch`
   },
@@ -165,7 +168,7 @@
     size1: `3 Count`,
     size2: `6 Count`,
     size3: `9 Count`,
-    priceSale: `6.39`,
+    priceSale: 6.39,
     priceRegular: `7.99`,
     category: `can`
   },
@@ -179,7 +182,7 @@
     size1: `55g`,
     size2: `109g`,
     size3: `155g`,
-    priceSale: `1.99`,
+    priceSale: 1.99,
     priceRegular: `2.29`,
     category: `can`
   },
@@ -193,7 +196,7 @@
     size1: `55g`,
     size2: `80g`,
     size3: `195g`,
-    priceSale: `1.69`,
+    priceSale: 1.69,
     priceRegular: `1.99`,
     category: `plastic container`
   },
@@ -207,13 +210,13 @@
     size1: `95g`,
     size2: `155g`,
     size3: `195g`,
-    priceSale: `0.89`,
+    priceSale: 0.89,
     priceRegular: `0.99`,
     category: `can`
   },
   {
     imgsrc :`img/can16.jpg`,
-    name: `Nature's Variety® Instinct® Ultimate Protein®`,
+    name: `Nature's Variety® Instinct® Protein®`,
     discription: `This recipe is guided by a belief in raw and packed with 90% protein from real meat, liver and eggs`,
     flavor1: `Duck`,
     flavor2: `Turkey`,
@@ -221,8 +224,8 @@
     size1: `95g`,
     size2: `155g`,
     size3: `195g`,
-    priceSale: `0.89`,
-    priceRegular: `0.99`,
+    priceSale: 0.89,
+    priceRegular: `1.99`,
     category: `pouch`
   },
   {
@@ -235,7 +238,7 @@
     size1: `95g`,
     size2: `155g`,
     size3: `195g`,
-    priceSale: `2.59`,
+    priceSale: 2.59,
     priceRegular: `2.89`,
     category: `can`
   },
@@ -249,7 +252,7 @@
     size1: `85g`,
     size2: `115g`,
     size3: `295g`,
-    priceSale: `2.05`,
+    priceSale: 2.05,
     priceRegular: `2.09`,
     category: `can`
   },
@@ -263,7 +266,7 @@
     size1: `68g`,
     size2: `139g`,
     size3: `207g`,
-    priceSale: `1.59`,
+    priceSale: 1.59,
     priceRegular: `1.99`,
     category: `plastic container`
   },
@@ -277,7 +280,7 @@
     size1: `99g`,
     size2: `159g`,
     size3: `309g`,
-    priceSale: `3.29`,
+    priceSale: 3.29,
     priceRegular: `3.49`,
     category: `plastic container`
   }
@@ -299,13 +302,17 @@
 
   function getProductAsHtmlString(product) {
 
+  let greatdeal;
+  if (product.priceSale < 1.50) {
+    greatdeal = `<small class="callout urgent">Super deal!</small>`;
+  }
+
     return `<article class="product">
     <div class="productimg"><a>
-      <div class="sale" style="background-image: url(img/sale.png);"></div>
       <img src="${product.imgsrc}" alt="Product Image"></a></div>
     
     
-    <h3><a>${product.name}</a></h3>
+    <h3><a>${product.name} ${greatdeal}</a></h3>
     <p>${product.discription}</p>
     <form>
       <fieldset>
@@ -334,23 +341,12 @@
         <data value="${product.priceSale}">
         <del>${product.priceRegular}</del> <ins><strong>${product.priceSale}</strong></ins>
         </data>
-        <button type="button"><span class="material-icons">add_shopping_cart</span> Add to Cart</button>
+        <button type="button" id="addOne"><span class="material-icons">add_shopping_cart</span> Add to Cart</button>
         <button type="button"><span class="material-icons">favorite</span>Add to List</button>
       </footer>
     </form>
   </article>`;
   }
-
-/*document.getElementById('products').innerHTML = getProductAsHtmlString(products[0])
-document.getElementById('products').innerHTML += getProductAsHtmlString(products[1])
-document.getElementById('products').innerHTML += getProductAsHtmlString(products[2])
-document.getElementById('products').innerHTML += getProductAsHtmlString(products[3])
-document.getElementById('products').innerHTML += getProductAsHtmlString(products[4])
-document.getElementById('products').innerHTML += getProductAsHtmlString(products[5])
-document.getElementById('products').innerHTML += getProductAsHtmlString(products[6])
-document.getElementById('products').innerHTML += getProductAsHtmlString(products[7])
-document.getElementById('products').innerHTML += getProductAsHtmlString(products[8])
-document.getElementById('products').innerHTML += getProductAsHtmlString(products[9])*/
 
 
 
@@ -378,3 +374,11 @@ document.getElementById('toggleView').addEventListener('click', toggleProductLay
 document.getElementById('courseCategory').addEventListener('change', loadProductCategory);
 
 
+let numItemsInCart = 0;
+
+function addOneToCart() {
+    numItemsInCart += 1;
+    document.getElementById(`cartNum`).innerHTML = `${numItemsInCart}`;
+}
+
+document.getElementById(`addOne`).addEventListener('click', addOneToCart);
