bece9c14fb022e9f7cec96e486b401386e8c5fc3
Sunhwa Cheon <Sunhwa@CheonSunhwaui-MacBook-Air.local>
2020-05-15 13:58:23 -0400

    0515

--- js/index.js
+++ js/index.js
@@ -8,12 +8,6 @@
     imgsrc :`img/can1.jpg`,
     name: `CRAVE™ Perfect Portions Adult Cat Food`,
     discription: `Real chicken is the first ingredient, and this protein-rich recipe provides cats with energy and supports lean bodies`,
-    flavor1: `Chicken`,
-    flavor2: `Turkey`,
-    flavor3: `Chicken & Beef`,
-    size1: `32g`,
-    size2: `79g`,
-    size3: `150g`,
     priceSale: 1.99,
     priceRegular: `2.11`,
     qty:785,
@@ -23,12 +17,6 @@
     imgsrc :`img/can2.jpg`,
     name: `Authority® Advanced Wellness Shredded`,
     discription: `This delicious wet food features the great taste of chicken in each and every bite`,
-    flavor1: `Mackerel`,
-    flavor2: `Salmon`,
-    flavor3: `Tuna`,
-    size1: `15.5g`,
-    size2: `35g`,
-    size3: `70g`,
     priceSale: 1.19,
     priceRegular: `1.99`,
     qty:245,
@@ -38,12 +26,6 @@
     imgsrc :`img/can3.jpg`,
     name: `Royal Canin® Feline Health Nutrition`,
     discription: `A uniquely balanced blend of vitamins and minerals promotes your pet's optimal health and wellness`,
-    flavor1: `Sensitive Digestion`,
-    flavor2: `Beauty Chunk`,
-    flavor3: `Nutrition Chunk`,
-    size1: `85g`,
-    size2: `120g`,
-    size3: `none`,
     priceSale: 1.89,
     priceRegular: `1.99`,
     qty:115,
@@ -53,12 +35,6 @@
     imgsrc :`img/can4.jpg`,
     name: `Wellness® CORE® Tiny Tasters Wet Cat Food`,
     discription: `Based on the nutritional philosophy that cats, given their primal ancestry, thrive on a diet mainly comprised of meat`,
-    flavor1: `Beef`,
-    flavor2: `Tuna & Chicken`,
-    flavor3: `Duck`,
-    size1: `60g`,
-    size2: `85g`,
-    size3: `none`,
     priceSale: 1.99,
     priceRegular: `2.19`,
     qty:635,
@@ -68,12 +44,6 @@
     imgsrc :`img/can5.jpg`,
     name: `Simply Nourish® Pate Wet Adult Cat Food`,
     discription: `Perfect choice nutritionally for your adult cat`,
-    flavor1: `Salmon`,
-    flavor2: `Shrimp`,
-    flavor3: `Tuna & Shrimp`,
-    size1: `85g`,
-    size2: `155g`,
-    size3: `350g`,
     priceSale: 2.35,
     priceRegular: `2.55`,
     qty:635,
@@ -83,12 +53,6 @@
     imgsrc :`img/can6.jpg`,
     name: `Simply Nourish® Kitten Chicken Stew`,
     discription: `Simply Nourish® Shreds & Flake Stew Wet Cat Food - Natural, Grain Free`,
-    flavor1: `Chicken`,
-    flavor2: `Salmon`,
-    flavor3: `Turkey`,
-    size1: `79g`,
-    size2: `129g`,
-    size3: ``,
     priceSale: 1.99,
     priceRegular: `2.05`,
     qty:735,
@@ -98,12 +62,6 @@
     imgsrc :`img/can7.jpg`,
     name: `Only Natural Pet® 5 Ingredient Soup Cat Food Topper`,
     discription: `You won't find any grains, thickeners or artificial ingredients in these tasty items! `,
-    flavor1: `Chicken & Whitefish`,
-    flavor2: `Pork`,
-    flavor3: `Turkey & Chicken`,
-    size1: `118g`,
-    size2: `258g`,
-    size3: ``,
     priceSale: 5.49,
     priceRegular: `5.99`,
     qty:35,
@@ -113,12 +71,6 @@
     imgsrc :`img/can8.jpg`,
     name: `Iams® Perfect Portions`,
     discription: `Healthy Kitten Food`,
-    flavor1: `Chicken`,
-    flavor2: `Shrimp`,
-    flavor3: `Tuna & Shrimp`,
-    size1: `75g`,
-    size2: `155g`,
-    size3: `350g`,
     priceSale: 1.04,
     priceRegular: `1.24`,
     qty:455,
@@ -128,12 +80,6 @@
     imgsrc :`img/can9.jpg`,
     name: `Beyond® Grain Free Adult Cat Food`,
     discription: `Duck is the #1 ingredient in this wet cat food, and it's accented with wholesome vegetables, including sweet potatoes and carrots`,
-    flavor1: `Duck & Sweet`,
-    flavor2: `Shrimp`,
-    flavor3: `Tuna & Shrimp`,
-    size1: `85g`,
-    size2: `155g`,
-    size3: `350g`,
     priceSale: 1.59,
     priceRegular: `2.55`,
     qty:965,
@@ -143,12 +89,6 @@
     imgsrc :`img/can10.jpg`,
     name: `Merrick® Backcountry ™ Pate Adult`,
     discription: `This delicious food is made using all-natural ingredients you can feel good about serving your cat`,
-    flavor1: `Chicken`,
-    flavor2: `Rabbit`,
-    flavor3: `Tuna & Salmon`,
-    size1: `85g`,
-    size2: `155g`,
-    size3: `350g`,
     priceSale: 3.39,
     priceRegular: `3.39`,
     qty:25,
@@ -158,12 +98,6 @@
     imgsrc :`img/can11.jpg`,
     name: `Simply Nourish® SOURCE ™ Meal Topper`,
     discription: `Natural, High Protein, Grain Free`,
-    flavor1: `Duck`,
-    flavor2: `Salmon`,
-    flavor3: `Tuna & Salmon`,
-    size1: `90g`,
-    size2: `110g`,
-    size3: `220g`,
     priceSale: 1.79,
     priceRegular: `1.89`,
     qty:735,
@@ -173,12 +107,6 @@
     imgsrc :`img/can12.jpg`,
     name: `HALO® Lil Bub Wet Cat Food Variety Pack`,
     discription: `Halo has partnered with the most amazing cat on the planet`,
-    flavor1: `Chicken`,
-    flavor2: `Salmon`,
-    flavor3: `Turkey`,
-    size1: `3 Count`,
-    size2: `6 Count`,
-    size3: `9 Count`,
     priceSale: 6.39,
     priceRegular: `7.99`,
     qty:955,
@@ -188,12 +116,6 @@
     imgsrc :`img/can13.jpg`,
     name: `Nature's Variety® Instinct® Limited Ingredient Diet`,
     discription: `Guided by the purity and simplicity of raw, essential nutrition your cat needs to thrive`,
-    flavor1: `Chicken`,
-    flavor2: `Salmon`,
-    flavor3: `Turkey`,
-    size1: `55g`,
-    size2: `109g`,
-    size3: `155g`,
     priceSale: 1.99,
     priceRegular: `2.29`,
     qty:575,
@@ -203,12 +125,6 @@
     imgsrc :`img/can14.jpg`,
     name: `Catit® Chicken Dinner`,
     discription: `Catit Chicken Dinner with Tuna and Kale is a dual-layered wet food, made of fresh ingredients served in luscious gravy`,
-    flavor1: `Tuna and Kale`,
-    flavor2: `Whitefish and Pumpkin`,
-    flavor3: `Tuna and Carrots`,
-    size1: `55g`,
-    size2: `80g`,
-    size3: `195g`,
     priceSale: 1.69,
     priceRegular: `1.99`,
     qty:45,
@@ -217,13 +133,7 @@
   {
     imgsrc :`img/can15.jpg`,
     name: `Authority® Pate Entree Kitten`,
-    discription: `Provide your kitten with the kind of nutritional support and great taste she deserves at mealtime with Authority Everyday Health Chicken Entree Kitten Food`,
-    flavor1: `Chicken`,
-    flavor2: `Whitefish`,
-    flavor3: `Tuna`,
-    size1: `95g`,
-    size2: `155g`,
-    size3: `195g`,
+    discription: `Provide your kitten with the kind of nutritional support and great taste she deserves at mealtime with Authority Entree Kitten Food`,
     priceSale: 0.89,
     priceRegular: `0.99`,
     qty:735,
@@ -233,12 +143,6 @@
     imgsrc :`img/can16.jpg`,
     name: `Nature's Variety® Instinct® Protein®`,
     discription: `This recipe is guided by a belief in raw and packed with 90% protein from real meat, liver and eggs`,
-    flavor1: `Duck`,
-    flavor2: `Turkey`,
-    flavor3: `Rabbit`,
-    size1: `95g`,
-    size2: `155g`,
-    size3: `195g`,
     priceSale: 0.89,
     priceRegular: `1.99`,
     qty:985,
@@ -247,13 +151,7 @@
   {
     imgsrc :`img/can17.jpg`,
     name: `Blue Buffalo Wilderness Denali Dinner`,
-    discription: `Make meal time delicious and nutritious for your kitty with Blue Buffalo Wilderness Wild Salmon, Venison and Halibut Denali Dinner Wet Cat Food`,
-    flavor1: `Salmon`,
-    flavor2: `Whitefish`,
-    flavor3: `Rabbit`,
-    size1: `95g`,
-    size2: `155g`,
-    size3: `195g`,
+    discription: `Make meal time delicious and nutritious for your kitty with Blue Buffalo Wilderness Wild Salmon, Venison`,
     priceSale: 2.59,
     priceRegular: `2.89`,
     qty:65,
@@ -263,12 +161,6 @@
     imgsrc :`img/can18.jpg`,
     name: `HALO® Kitten Food`,
     discription: `Halo Grain Free Whitefish Recipe Kitten Food uses WHOLE meat, never any "meat meal" of ANY kind in our natural cat food`,
-    flavor1: `Salmon`,
-    flavor2: `Whitefish`,
-    flavor3: `Rabbit`,
-    size1: `85g`,
-    size2: `115g`,
-    size3: `295g`,
     priceSale: 2.05,
     priceRegular: `2.09`,
     qty:465,
@@ -278,12 +170,6 @@
     imgsrc :`img/can19.jpg`,
     name: `Nutro Wild Frontier Adult Cat Food`,
     discription: `Natural, grain free`,
-    flavor1: `Salmon & Trout`,
-    flavor2: `Chicken & Beef`,
-    flavor3: `Turkey & Vegetable`,
-    size1: `68g`,
-    size2: `139g`,
-    size3: `207g`,
     priceSale: 1.59,
     priceRegular: `1.99`,
     qty:345,
@@ -293,12 +179,6 @@
     imgsrc :`img/can20.jpg`,
     name: `Nature's Variety® Instinct® Grain Free`,
     discription: `Feed your adult cat 1-1/2 to 2 bowls per 6 to 8 lb of body weight per day. Refrigerate unused portion.`,
-    flavor1: `Salmon`,
-    flavor2: `Chicken`,
-    flavor3: `Kangaroo`,
-    size1: `99g`,
-    size2: `159g`,
-    size3: `309g`,
     priceSale: 3.29,
     priceRegular: `3.49`,
     qty:5,
@@ -344,6 +224,8 @@
     lowstock = `\n`
   }
 
+
+
     return `<article class="product">
     <div class="productimg"><a>
       <img src="${product.imgsrc}" alt="Product Image"></a>${greatdeal} ${lowstock}</div>
@@ -351,24 +233,8 @@
     
     <h3><a>${product.name} </a></h3>
     <p>${product.discription}</p>
-    <form>
-      <fieldset>
-        <legend>Flavor</legend>
-        <ul>
-          <li><label><input type="radio" name="Flavor" value="${product.flavor1}"> <span>${product.flavor1}</span></label></li>
-          <li><label><input type="radio" name="Flavor" value="${product.flavor2}"> <span>${product.flavor2}</span></label></li>
-          <li><label><input type="radio" name="Flavor" value="${product.flavor3}"> <span>${product.flavor3}</span></label></li>
-        </ul>
-      </fieldset>
-
-      <fieldset>
-        <legend>Sizes</legend>
-        <ol>
-            <li><label><input type="radio" name="size" value="${product.size1}g"> <span>${product.size1}</span></label></li>
-            <li><label><input type="radio" name="size" value="${product.size2}g"> <span>${product.size2}</span></label></li>
-            <li><label><input type="radio" name="size" value="${product.size3}g"> <span>${product.size3}</span></label></li>
-        </ol>
-      </fieldset>
+    <form class="disappear">
+      
 
       <footer>
         <dl>
@@ -378,8 +244,8 @@
         <data value="${product.priceSale}">
         <del>${product.priceRegular}</del> <ins><strong>${product.priceSale}</strong></ins>
         </data>
-        <button type="button" onclick = "addOneToCart()" id="addOne"><span class="material-icons">add_shopping_cart</span> Add to Cart</button>
-        <button type="button"><span class="material-icons">favorite</span>Add to List</button>
+        <button type="button" onclick = "addOneToCart()" id="addOne"><span class="material-icons">add_shopping_cart</span></button>
+        <button type="button" onclick = "addOneToList()" id="addList"><span class="material-icons">favorite</span></button>
       </footer>
     </form>
   </article>`;
@@ -401,6 +267,11 @@ function toggleProductLayout(event) {
   document.getElementById('products').classList.toggle('grid-view');
 }
 
+function disappear(event){
+
+  document.querySelector('.disappear').style.display = 'none';
+}
+
 function loadProductCategory(event) {
   const categoryImSearchingFor = event.target.value;
   renderProducts(products.filter(isInThisCategory, categoryImSearchingFor)); 
@@ -413,9 +284,38 @@ function loadProductByName(event) {
 
 
 renderProducts(products);
+
+
+// 1.
+// document.getElementById('toggleView').addEventListener('click', function(e) { 
+//   const tgt = e.target; 
+//   if (tgt.classList.contains("toggleView")) {
+//     const parent = this.closest("form");
+//     parent.querySelector(".disappear").style.display="none";
+//     parent.querySelector("#products").classList.toggle('grid-view'); }
+//   })
+
+
+
+// 2.
+// document.getElementById('toggleView').addEventListener('click',()=>{
+//   if(document.getElementById('products').classList='grid-view'){
+//     document.querySelector('.disappear').style.display='none'
+//   }else{
+//     document.querySelector('.disappear').style.display='block'
+//   }
+  
+// } 
+// )
+ 
+
+// Original!!!!
 document.getElementById('toggleView').addEventListener('click', toggleProductLayout);
 document.getElementById('courseCategory').addEventListener('change', loadProductCategory);
 document.getElementById('search').addEventListener('input', loadProductByName);
+
+
+
 
 
 let numItemsInCart = 0;
@@ -427,3 +327,24 @@ function addOneToCart() {
 
 document.getElementById(`addOne`).addEventListener('click', addOneToCart);
 
+
+function addOneToList(){
+  document.getElementById(`listNum`).innerHTML = `favorite`;
+  alert(`1 item added to Wish List`)
+}
+
+document.getElementById(`addList`).addEventListener('click',addOneToList);
+
+
+// var items = document.getElementsByClassName('item');
+// var dim = document.querySelector('html');
+
+// for (var i = 0; i < items.length; i++) {
+//   items[i].addEventListener('mouseover', function () {
+//     this.style.opacity = '1';
+//     dim.style.opacity = '0.5';
+//     });
+//     items[i].addEventListener('mouseleave', function () {
+//       dim.style.backgroundColor = 'white';
+//     });
+// }
